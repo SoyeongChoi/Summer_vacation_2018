0a71c6afb0efb93982e4a34c6122f334b3b7479d
SoyeongChoi <201602084@cnu.ac.kr>
2018-06-25 18:15:22 +0900

    백준11718

--- Vacation_Algorithm/src/al_180625/al_11718.java
+++ Vacation_Algorithm/src/al_180625/al_11718.java
@@ -1,5 +1,11 @@
 package al_180625;
 
-public class al_11718 {
+import java.io.BufferedReader;
+import java.util.Scanner;
 
+public class al_11718 {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in); 
+		BufferedReader bf = new BufferedReader(bf);
+	}
 }
