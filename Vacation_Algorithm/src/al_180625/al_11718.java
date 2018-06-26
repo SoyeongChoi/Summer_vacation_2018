13df9b26d255d3dddb6bc53057136598153b9cee
SoyeongChoi <201602084@cnu.ac.kr>
2018-06-26 14:43:43 +0900

    간단하게 바로입력입력으로 바꿈

--- Vacation_Algorithm/src/al_180625/al_11718.java
+++ Vacation_Algorithm/src/al_180625/al_11718.java
@@ -11,32 +11,11 @@ import java.util.Scanner;
 
 public class al_11718 {
 	public static void main(String[] args) {
-		   ClassLoader cloader = al_11718.class.getClassLoader();
-	          
-		File file;
-		while(true){
-			try {
-				file = new File(cloader.getResource("al_180625/text.txt").getFile());
-				BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));// bufferedwriterë¥?
-																							// ?†µ?•´
-																							// ?…? ¥ë°›ìŒ
-				if (file.isFile() && file.canWrite()) {// ?ŒŒ?¼?´ê³?, ?“¸ ?ˆ˜ ?ˆ?œ¼ë©?
-					Scanner input = new Scanner(System.in);// ?…? ¥ë°›ìŒ
-					bufferedWriter.write(input.nextLine());// ?ŒŒ?¼?— ?“´?‹¤.
-					bufferedWriter.close();// ë²„í¼ ?‹«?•„?„œ ì¢…ë£Œ?‹œ?‚´
-				}
-				BufferedReader br = new BufferedReader(new FileReader(new File(cloader.getResource("al_180625/text.txt").getFile())));// br?—
-																										// ?ŒŒ?¼?„
-																										// ?„£?–´ì£¼ê³ 
-																										// ?½?–´ì¤?
-				String a = br.readLine();
-				System.out.println(a);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
+		Scanner input = new Scanner(System.in);
+		while(input.hasNextLine()){
+			String a = input.nextLine();
+			System.out.println(a);
 		}
-	
+		
 	}
 }
