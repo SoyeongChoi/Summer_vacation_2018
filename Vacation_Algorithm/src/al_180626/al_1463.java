71171066800d1ac6bb8ec0232abf5697ec210467
SoyeongChoi <201602084@cnu.ac.kr>
2018-06-26 15:01:17 +0900

    codeground기출문제로 변경 정수정렬하는 문제 풀기

---
+++ Vacation_Algorithm/src/al_180626/al_1463.java
@@ -0,0 +1,16 @@
+package al_180626;
+
+import java.util.Scanner;
+
+public class al_1463 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		while(input.hasNextLine()){
+			String a = input.nextLine();
+			System.out.println(a);
+		}
+	}
+
+}
