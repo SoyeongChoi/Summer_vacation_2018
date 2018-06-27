067d8e8da38be5775bdbe7a1d6683154fc3bb9a3
SoyeongChoi <201602084@cnu.ac.kr>
2018-06-27 17:50:10 +0900

    점화식 이용해서 해결

--- Vacation_Algorithm/src/al_180627/freelancer.java
+++ Vacation_Algorithm/src/al_180627/freelancer.java
@@ -20,54 +20,52 @@ import java.util.Scanner;
 class freelancer {
 	static int Answer;
 
-	public static void main(String args[]) throws Exception	{
+	public static void main(String args[]) throws Exception {
 		/*
-		   The method below means that the program will read from input.txt, instead of standard(keyboard) input.
-		   To test your program, you may save input data in input.txt file,
-		   and call below method to read from the file when using nextInt() method.
-		   You may remove the comment symbols(//) in the below statement and use it.
-		   But before submission, you must remove the freopen function or rewrite comment symbols(//).
-		 */		
+		 * The method below means that the program will read from input.txt,
+		 * instead of standard(keyboard) input. To test your program, you may
+		 * save input data in input.txt file, and call below method to read from
+		 * the file when using nextInt() method. You may remove the comment
+		 * symbols(//) in the below statement and use it. But before submission,
+		 * you must remove the freopen function or rewrite comment symbols(//).
+		 */
 
 		/*
-		   Make new scanner from standard input System.in, and read data.
+		 * Make new scanner from standard input System.in, and read data.
 		 */
 		Scanner sc = new Scanner(System.in);
-		//Scanner sc = new Scanner(new FileInputStream("input.txt"));
+		// Scanner sc = new Scanner(new FileInputStream("input.txt"));
 
 		int T = sc.nextInt();
-		for(int test_case = 0; test_case < T; test_case++) {
-			 Answer = 0;
+		for (int test_case = 0; test_case < T; test_case++) {
+			Answer = 0;
 			int N = sc.nextInt();
 			ArrayList<Integer> p = new ArrayList<Integer>();
 			ArrayList<Integer> q = new ArrayList<Integer>();
-			
-			for(int i = 0; i < N; i++){
+			p.add(0);
+			q.add(0);
+			for (int i = 0; i < N; i++) {
 				p.add(sc.nextInt());
 			}
-			for(int i = 0; i < N; i++){
+			for (int i = 0; i < N; i++) {
 				q.add(sc.nextInt());
 			}
-			int q_max = (int) Collections.max(q);
-			for(int i = 0; i < N; i+=2){
-				int p_val = p.get(i)+p.get(i+1);
-				int q_val = q.get(i+1);
-				if(p_val> q_val){
-					Answer+=p_val;
-				}else{
-					Answer+=q_val;
-				}
+			int reference[] = new int[N+1];
+			reference[0]= 0;
+			reference[1] = Math.max(p.get(0), q.get(0));
+			for (int i = 2; i <= N; i++) {
+				reference[i] = Math.max(reference[i-1]+p.get(i), reference[i-2]+q.get(i));
 			}
+			Answer = reference[N];
 			/////////////////////////////////////////////////////////////////////////////////////////////
 			/*
-			   Implement your algorithm here.
-			   The answer to the case will be stored in variable Answer.
+			 * Implement your algorithm here. The answer to the case will be
+			 * stored in variable Answer.
 			 */
 			/////////////////////////////////////////////////////////////////////////////////////////////
 
-
 			// Print the answer to standard output(screen).
-			System.out.println("Case #"+(test_case+1));
+			System.out.println("Case #" + (test_case + 1));
 			System.out.println(Answer);
 		}
 	}
